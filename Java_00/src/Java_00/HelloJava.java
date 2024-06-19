4b26502c34dd7d40a5553b10757ba62e2c565b2b
hayley0903 <hyejeonglee0903@gmail.com>
2024-06-19 09:28:16 +0900

    <2024.06.19> - 최초 커밋

---
+++ Java_00/src/Java_00/HelloJava.java
@@ -0,0 +1,19 @@
+package Java_00;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("Hello Java 1 !!!");
+		
+		System.out.println("Hello Java 2 !!!");
+		
+		System.out.println("Hello Java 3 !!!");
+		
+		String test1 = "안녕하세요 저는 이혜정입니다.";
+		
+		System.out.print(test1);
+		
+	}
+}
