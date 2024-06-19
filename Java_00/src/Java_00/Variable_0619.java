58f8e9472073318b7d9ce6933a358c5db0edeefc
hayley0903 <hyejeonglee0903@gmail.com>
2024-06-19 12:15:26 +0900

    <2024.06.19>
    - variable 변수형 예제

---
+++ Java_00/src/Java_00/Variable_0619.java
@@ -0,0 +1,118 @@
+package Java_00;
+
+public class Variable_0619 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/* 범위 주석 처리 
+		 int varNum = 5;
+		
+		//String varString ="5";
+		
+		int varNumber;
+		varNumber = 100;
+		
+		int varNumResult;
+		varNumResult = varNum + varNumber;
+		
+		System.out.println("숫자 변수 1 {" + varNum + "} 입니다.");
+		System.out.println("숫자 변수 2 {" + varNumber + "} 입니다.");
+		System.out.println(varNumResult);
+		
+		System.out.println("숫자 1과 숫자2를 더하면? {" + varNum + varNumber + "} 입니다.");
+		
+		
+		//괄호 안의 숫자변수를 미리 더해줌
+		System.out.println("숫자 1과 숫자2를 더하면? {" + (varNum + varNumber) + "} 입니다.");
+		
+		System.out.println("숫자 1과 숫자2를 더하면? {" + varNumResult + "} 입니다."); 
+		*/
+		
+		//변수 값 바꾸기
+		
+		/*int num1 = 10;
+		int num2 = 20;
+		
+		int numResult;
+		
+		System.out.println(" === 숫자 바꾸기 전 ===");
+		System.out.println("숫자 1 {" + num1 + "}");
+		System.out.println("숫자 2 {" + num2 + "}");
+		//System.out.println(" 더한 값 {" + numResult + "}" );
+		
+		numResult = num1;
+		num1 = num2;
+		num2 = numResult;
+		
+		System.out.println(" === 숫자 바꾸기 후 ===");
+		System.out.println("숫자 1 {" + num1 + "}");
+		System.out.println("숫자 2 {" + num2 + "}");
+		
+		
+		//변수값 3개 바꾸기
+		
+		int var1 = 10;
+		int var2 = 20;
+		int var3 = 30;
+		
+		System.out.println(" === 숫자 바꾸기 전 ===");
+		System.out.println("숫자 1 {" + var1 + "}");
+		System.out.println("숫자 2 {" + var2 + "}");
+		System.out.println("숫자 3 {" + var3 + "}");
+		
+		int varResult;
+		
+		varResult = var1;
+		var1 = var2;
+		var2 = var3;
+		var3 = varResult;
+		
+		System.out.println(" === 숫자 바꾸기 후 ===");
+		System.out.println("숫자 1 {" + var1 + "}");
+		System.out.println("숫자 2 {" + var2 + "}");
+		System.out.println("숫자 3 {" + var3 + "}");
+		*/
+		
+		//년도와 나이 계산
+		
+		int curYear = 2024;
+		int curAge = 45;
+		
+		int varNum = 1;
+		
+		int yearResult;
+		int ageResult;
+		
+		
+		
+		yearResult = curYear + varNum;
+		ageResult = curAge + varNum;
+		
+		System.out.println("올해는 {" + curYear + "} 년도 입니다.");
+		System.out.println("올해 내 나이는 {" + curAge + "} 세 입니다.");
+		
+		System.out.println("다음해는 {" + yearResult + "} 년도 입니다.");
+		System.out.println("올해 내 나이는 {" + ageResult + "} 세 입니다.");
+		
+		
+		yearResult = yearResult + varNum;
+		ageResult = ageResult + varNum;
+		
+		System.out.println("다다음해는 {" + yearResult + "} 년도 입니다.");
+		System.out.println("올해 내 나이는 {" + ageResult + "} 세 입니다.");
+		
+		yearResult = yearResult + varNum;
+		ageResult = ageResult + varNum;
+		
+		System.out.println("다다다음해는 {" + yearResult + "} 년도 입니다.");
+		System.out.println("올해 내 나이는 {" + ageResult + "} 세 입니다.");
+		
+		
+		
+		
+		
+		
+	}
+	
+}
